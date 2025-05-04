df5bb7ae0223c0c5e33ea784640d107482618591
Gaspar Menesses <menessesgaspar@gmail.com>
2025-05-04 16:37:43 -0300

    ataque y defensa

--- src/Program/Program.cs
+++ src/Program/Program.cs
@@ -6,10 +6,10 @@ class Program
         
         // Creo dos personajes para que se enfrenten
         var jugador  = CharacterPrompter.Prompt("Selecciona tu personaje");
-        Console.WriteLine($"Has creado a el {jugador.Race} {jugador.Name} con {jugador.CurrentHealth} de vida.");
+        Console.WriteLine($"Has creado a el {jugador.Race} {jugador.Name} con {jugador.CurrentHealth} de vida, {jugador.Ataque} de ataque y {jugador.Defensa} de defensa");
 
         var oponente = CharacterPrompter.Prompt("Selecciona un oponente");
-        Console.WriteLine($"Has creado a el {oponente.Race} {oponente.Name} con {oponente.CurrentHealth} de vida.");
+        Console.WriteLine($"Has creado a el {oponente.Race} {oponente.Name} con {oponente.CurrentHealth} de vida, {oponente.Ataque} de ataque y {oponente.Defensa} de defensa");
         
     }
 }
