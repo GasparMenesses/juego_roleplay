741fba6a589109322de1467514c1740fc8e9dfc3
Gaspar Menesses <menessesgaspar@gmail.com>
2025-05-04 19:07:08 -0300

    arreglando cosas

--- src/Library/Personajes/Elve.cs
+++ src/Library/Personajes/Elve.cs
@@ -7,4 +7,6 @@ public class Elve : Character
     public Elve(string name)  //Ese : base(...) le dice al compilador “antes de ejecutar el cuerpo de este constructor, invoca el constructor de la clase padre (Character) pasándole estos parámetros”
         : base("Elfo", name, maxHealth.Next(70, 101), 10, 5)   
     { }
+
+    public static int sape = maxHealth.Next(10, 80);
 }
