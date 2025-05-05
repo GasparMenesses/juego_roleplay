7c9a4c7b072cd0877da824985db6d0f8c636e997
Joaquin de Dios <joaquin.dedios@correo.ucu.edu.uy>
2025-05-05 14:47:48 -0300

    comentarios en los personajes

--- src/Library/Personajes/Wizard.cs
+++ src/Library/Personajes/Wizard.cs
@@ -1,9 +1,9 @@
 ﻿namespace Library.Personajes;
 
-public class Wizard : Character
+public class Wizard : Character // la clase Elve hereda de la clase character
 {
-    private static readonly Random maxHealth = new Random();
+    private static readonly Random maxHealth = new Random(); //usamos random static para inicializar la salud en el valor máximo
     public Wizard(string name)  //Ese : base(...) le dice al compilador “antes de ejecutar el cuerpo de este constructor, invoca el constructor de la clase padre (Character) pasándole estos parámetros”
         : base("Wizard", name, maxHealth.Next(70, 101), 10, 5)    
     { }
-}
+}       //Se le asigna un tipo ("Wizard"), un nombre, una salud máxima aleatoria entre 70 y 100 y valores fijos de ataque (10) y defensa (5).
