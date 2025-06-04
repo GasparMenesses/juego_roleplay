17f73a31830fed5ee66f43fd40dac3a21884aaaa
Joaquin de Dios <joaquin.dedios@correo.ucu.edu.uy>
2025-06-04 20:41:58 -0300

    arreglando cosas

--- src/Library/Personajes/Enemigos/Shrek.cs
+++ src/Library/Personajes/Enemigos/Shrek.cs
@@ -5,13 +5,13 @@ namespace Library.Personajes.Enemigos;
 public class Shrek : Enemy
 {
     
-    private static readonly Random maxHealth = new Random(); //usamos random static para inicializar la salud en el valor máximo
+    private static readonly Random maxHealth = new Random(); // usamos random static para inicializar la salud en el valor máximo
     private static readonly Random Wp = new Random();
-    public Shrek(string name)  //Ese : base(...) le dice al compilador “antes de ejecutar el cuerpo de este constructor, invoca el constructor de la clase padre (Character) pasándole estos parámetros”
+    public Shrek(string name)  // Ese : base(...) le dice al compilador “antes de ejecutar el cuerpo de este constructor, invoca el constructor de la clase padre (Character) pasándole estos parámetros”
         : base(name, "Enemy", maxHealth.Next(70, 101), 10, 5,Wp.Next(3,20))   
     { }
 
     public static int vida = maxHealth.Next(10, 80); //Campo estático llamado 'vida' que se inicializa con un valor aleatorio entre 10 y 79.
-    //Este valor se comparte entre todas las instancias de Shrek.
+    // Este valor se comparte entre todas las instancias de Shrek.
     public static int puntosvictoria = Wp.Next(3, 20);
 }
