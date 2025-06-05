01498f46d154566111007bd63c6713dbb40afbfa
Agustin Pechi <AGUSTIN.PECHI@correo.ucu.edu.uy>
2025-06-04 21:02:52 -0300

    Agrego test

--- test/LibraryTests/ExampleTest.cs
+++ test/LibraryTests/ExampleTest.cs
@@ -1,21 +1,24 @@
 using Library;
 using Library.Elementos;
+using Library.Personajes.Enemigos;
 using Library.Personajes.Heroes;
 
 namespace LibraryTests;
 
 public class Tests
 {
-    private Hero _character1;
-    private Hero _character2;
+    private Hero _hero1;
+    private Hero _hero2;
+    private Enemy _enemy1;
     /// <summary>
     /// Inicializa dos personajes: un Enano y un Mago
     /// </summary>
     [SetUp]
     public void Setup()
     { 
-        _character1 = new Dwarve("Juan");
-        _character2 = new Wizard("MiniMago");
+        _hero1 = new Dwarve("Juan");
+        _hero2 = new Wizard("MiniMago");
+        _enemy1 = new Shrek("Shrek");
     }
     /// <summary>
     /// Este test verifica que un personaje que no puede equipar un SpellBook
@@ -24,8 +27,8 @@ public class Tests
     [Test]
     public void FalseEquipTest()
     { 
-        Item.SpellBook spellbook = new Item.SpellBook(_character1);
-        Assert.IsFalse(_character1.Actions.AddItem(spellbook),"Deberia ser false");
+        Item.SpellBook spellbook = new Item.SpellBook(_hero1);
+        Assert.IsFalse(_hero1.Actions.AddItem(spellbook),"Deberia ser false");
     }
     /// <summary>
     /// Este test verifica que un personaje que si puede equipar un SpellBook 
@@ -34,8 +37,8 @@ public class Tests
     [Test]
     public void TrueEquipTest()
     {
-        Item.SpellBook spellBook = new Item.SpellBook(_character2);
-        Assert.IsTrue(_character2.Actions.AddItem(spellBook),"Deberia ser true");
+        Item.SpellBook spellBook = new Item.SpellBook(_hero2);
+        Assert.IsTrue(_hero2.Actions.AddItem(spellBook),"Deberia ser true");
     }
     /// <summary>
     /// Este test comprueba que al equipar un ítem ofensivo
@@ -44,10 +47,10 @@ public class Tests
     [Test]
     public void ChangeStats()
     {
-        int beforeEquip = _character1.Attack;
+        int beforeEquip = _hero1.Attack;
         Item.Axe axe = new Item.Axe();
-        _character1.Actions.AddItem(axe);
-        int afterEquip = _character1.Attack;
+        _hero1.Actions.AddItem(axe);
+        int afterEquip = _hero1.Attack;
         Assert.That(afterEquip, Is.Not.EqualTo(beforeEquip), "El ataque no cambio despues de equipar el hacha");
     }
     /// <summary>
@@ -57,9 +60,9 @@ public class Tests
     [Test]
     public void TakeDamage()
     {
-        int maxHealth = _character2.MaxHealth;
-        _character1.Actions.Attack(_character2);
-        int currentHealth = _character2.CurrentHealth;
+        int maxHealth = _hero2.MaxHealth;
+        _hero1.Actions.Attack(_hero2);
+        int currentHealth = _hero2.CurrentHealth;
         Assert.That(currentHealth, Is.Not.EqualTo(maxHealth), "La vida actual no se redujo despues de ser atacado");
     }
     /// <summary>
@@ -69,46 +72,49 @@ public class Tests
     [Test]
     public void AutoHeal()
     {
-        _character1.CurrentHealth = 1;
-        _character1.Actions.Heal();
-        Assert.That(_character1.CurrentHealth, Is.Not.EqualTo(1), "La vida actual no aumento despues de curarse");
+        _hero1.CurrentHealth = 1;
+        _hero1.Actions.Heal();
+        Assert.That(_hero1.CurrentHealth, Is.Not.EqualTo(1), "La vida actual no aumento despues de curarse");
     }
-    
     /// <summary>
     /// Este test comprueba si la vida de el personaje al ser atacado disminuye a cero correctamente
     /// </summary>
-
     [Test]
     public void CharacterDie()
     {
-        _character2.CurrentHealth = 1;
-        _character1.Actions.Attack(_character2);
-        Assert.That(_character2.CurrentHealth, Is.EqualTo(0));
+        _hero2.CurrentHealth = 1;
+        _hero1.Actions.Attack(_hero2);
+        Assert.That(_hero2.CurrentHealth, Is.EqualTo(0));
     }
-    
     /// <summary>
     /// Este test comprueba si la vida de el personaje al ser atacado disminuye a menos de cero
     /// </summary>
     [Test]
     public void LifeIsntLessThanZero()
     {
-        _character2.CurrentHealth = 1;
-        _character1.Actions.Attack(_character2);
-        Assert.That(_character2.CurrentHealth, Is.EqualTo(0));
+        _hero2.CurrentHealth = 1;
+        _hero1.Actions.Attack(_hero2);
+        Assert.That(_hero2.CurrentHealth, Is.EqualTo(0));
     }
-    
-    
     /// <summary>
     /// Este test comprueba si la vida de el personaje muerto al ser atacado disminuye a menos de cero
     /// </summary>
-    
     [Test]
     public void CharacterDieAtack()
     {
-        _character2.CurrentHealth = 0;
-        _character1.Actions.Attack(_character2);
-        Assert.That(_character2.CurrentHealth, Is.EqualTo(0));
+        _hero2.CurrentHealth = 0;
+        _hero1.Actions.Attack(_hero2);
+        Assert.That(_hero2.CurrentHealth, Is.EqualTo(0));
     }
-    
-    
+    /// <summary>
+    /// Este test comprueba si cuando un heroe vence a un enemigo gana wp
+    /// </summary>
+    [Test]
+    public void HeroWinWp()
+    {
+        int currentwp = _hero1.CuerrentWp;
+        _enemy1.CurrentHealth = 1;
+        _hero1.Actions.Attack(_enemy1);
+        Assert.That(currentwp, Is.LessThan(0));
+    }
 }
